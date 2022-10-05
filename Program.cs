48b23602d342c9e5aed57d75ab6de768d808c3ea
Esbetron <elsarell1000@gmail.com>
2022-10-05 11:24:14 +1100

    post

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+var incrementer = 0;
+var stopValue =10; //allows us to control and change this variable to what ever number we want 
+
+while (incrementer <= stopValue){
+    System.Console.WriteLine(incrementer);
+    incrementer++;
+
+}
